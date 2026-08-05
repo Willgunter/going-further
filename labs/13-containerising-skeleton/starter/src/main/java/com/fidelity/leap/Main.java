c9e1cb08e958e919c753cbc8784c91b15a1976ff
Zack Todd <zack.todd@watchelm.com>
2026-08-05 17:48:36 +0100

    Completed labs and bug fixes

--- labs/13-containerising-skeleton/starter/src/main/java/com/fidelity/leap/Main.java
+++ labs/13-containerising-skeleton/starter/src/main/java/com/fidelity/leap/Main.java
@@ -3,6 +3,6 @@ package com.fidelity.leap;
 public class Main {
     public static void main(String[] args) {
         // TODO: replace <team-name> with your team's actual name
-        System.out.println("Hello world from <team-name>'s Sprint 1 project skeleton");
+        System.out.println("Hello world from Team Fidelity's Sprint 1 project skeleton");
     }
 }
