767a13406bcbf55fc845fbac42a7831aef27662d
Will Gunter <jameswgunterlll@gmail.com>
2026-08-20 20:04:16 +0000

    health

--- labs/11-docker-fundamentals/starter/src/main/java/com/neueda/leap/Main.java
+++ labs/11-docker-fundamentals/starter/src/main/java/com/neueda/leap/Main.java
@@ -5,6 +5,6 @@ public class Main {
         Greeter greeter = new Greeter();
         System.out.println(greeter.greet("Sprint 1"));
         System.out.println("Container is up. Sleeping so you can docker ps / docker logs / docker exec into it.");
-        Thread.sleep(600_000);
+        Thread.sleep(6_000);
     }
 }
